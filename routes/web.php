704f3e97aa801d7f46f702d96fa8d20949564cb4
DUWIPANGGA <aryad2232@gmail.com>
2025-11-21 22:00:12 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -44,8 +44,9 @@ use App\Http\Controllers\CustomSectionController;
     Route::get('card', function () {
         return view('card');
     });
-
+    
     Route::middleware('auth')->group(function () {
+        Route::resource('sertifikat', CertificateController::class);
             Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::patch('/profile{', [ProfileController::class, 'update'])->name('profile.update');
@@ -75,7 +76,6 @@ use App\Http\Controllers\CustomSectionController;
             Route::resource('seo-metas', SeoMetaController::class);
             Route::resource('page-views', PageViewController::class);
             Route::resource('development_processes', DevelopmentProcessController::class);
-    Route::resource('sertifikat', CertificateController::class);
     Route::post('/sertifikat/{certificate}/toggle-active', [CertificateController::class, 'toggleActive'])->name('certificates.toggle-active');
     Route::post('/sertifikat/update-order', [CertificateController::class, 'updateOrder'])->name('certificates.update-order');
         });
